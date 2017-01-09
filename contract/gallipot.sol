d75b96141f3cfe26a88f47e07d47421c1e38f9e4
manojpramesh <manojpramesh@gmail.com>
2017-01-09 13:45:23 +0530

    intial commit

---
+++ contract/gallipot.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+contract Gallipot {
+     
+    address creator;
+    string tokenName;
+    uint256 totalSupply;
+    uint256 unitCharge;
+
+    mapping(address => uint256) public balance;
+
+     function Gallipot(uint256 _initialSupply, string _tokenName, uint256 _unitHourCharge) {
+        creator = msg.sender;
+        tokenName = _tokenName;
+        totalSupply = _initialSupply;
+        balance[msg.sender] = _initialSupply;
+        unitCharge = _unitHourCharge;
+     }
+}
