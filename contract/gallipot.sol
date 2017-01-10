9ff13b3b5945510216a1deb848fb47011f678757
manojpramesh <manojpramesh@gmail.com>
2017-01-10 12:35:53 +0530

    add and verify items

--- contract/gallipot.sol
+++ contract/gallipot.sol
@@ -20,9 +20,20 @@ contract Gallipot {
          string name;
          address uia;
          address owner;
+         bool isVerified;
      }
 
      mapping (address => item) directory;
 
-     
+     function addItem(address _address, address _owner, string _name) {   
+         directory[_address].name = _name;
+         directory[_address].uia = _address;
+         directory[_address].owner = _owner;
+         directory[_address].isVerified = false;
+     }
+
+     function verifyItem(address _address, bool _isGenuine) {
+         if(msg.sender != owner) return;
+         directory[_address].isVerified = _isGenuine;
+     }
 }
