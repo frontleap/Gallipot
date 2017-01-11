ffe06569e9c69971a139d8a9704c3d4935b53524
manojpramesh <manojpramesh@gmail.com>
2017-01-11 15:06:55 +0530

    trace and edit item

--- contract/gallipot.sol
+++ contract/gallipot.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.0;
 contract Gallipot {
-     
+    
     address creator;
     string tokenName;
     uint256 totalSupply;
@@ -21,19 +21,26 @@ contract Gallipot {
          address uia;
          address owner;
          bool isVerified;
+         string[] trace;
      }
 
-     mapping (address => item) directory;
+     mapping (address => item) public directory;
 
-     function addItem(address _address, address _owner, string _name) {   
+     function addItem(address _address, address _owner, string _name, string _origin) {   
          directory[_address].name = _name;
          directory[_address].uia = _address;
          directory[_address].owner = _owner;
          directory[_address].isVerified = false;
+         directory[_address].trace.push(_origin);
      }
 
      function verifyItem(address _address, bool _isGenuine) {
-         if(msg.sender != owner) return;
+         if(msg.sender != creator) return;
          directory[_address].isVerified = _isGenuine;
+     }
+
+     function editName(address _address, string _name) {
+         if(msg.sender != creator || msg.sender != directory[_address].owner) return;
+         directory[_address].name = _name;
      }
 }
